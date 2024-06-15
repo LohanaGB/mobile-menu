60806452e21a8a73867fe48942bdee0c311703bc
Lohana <lohana.gvsp@gmail.com>
2024-06-15 00:31:10 -0300

    adjustments

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,24 @@
 <header class="header">
-    conteudo
+    <div class="container">
+        <div class="header__box">
+            <div class="row">
+
+                <div class="col-md-10">
+                    <div class="header__box__menuBox">
+                        <ul class="header__box__menuBox__menuList">
+                            <li class="header__box__menuBox__menuList__item"><a href="#">HOME</a></li>
+                            <li class="header__box__menuBox__menuList__item"><a href="#">ABOUT</a></li>
+                            <li class="header__box__menuBox__menuList__item"><a href="#">SERVICES</a></li>
+                            <li class="header__box__menuBox__menuList__item"><a href="#">PORTFOLIO</a></li>
+                            <li class="header__box__menuBox__menuList__item"><a href="#">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2">
+                </div>
+
+            </div>
+        </div>
+    </div>
 </header>
---
+++ includes/scripts.php
@@ -0,0 +1,12 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+
+<?php 
+$version = time();
+echo
+'
+<script type="text/javascript" src="scripts/plugins/slick.js?'.$version.'"></script>
+<script type="text/javascript" src="https://momentjs.com/downloads/moment.min.js?"></script>
+<script type="text/javascript" src="scripts/common.js?'.$version.'"></script>
+'; 
+?>
