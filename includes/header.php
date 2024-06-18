fde825dab7fb1644aa7db2005e2d0328d5ea0b6f
Lohana <lohana.gvsp@gmail.com>
2024-06-18 15:56:43 -0300

    adjustments

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,37 @@
 <header class="header">
     <div class="container">
         <div class="header__box">
-            <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                        <p class="header__box__social">
+                            <span class="icon icon-instagram"><a href="#"></a></span>
+                        </p>
+                    </div>
 
-                <div class="col-md-10">
-                    <div class="header__box__menuBox">
-                        <ul class="header__box__menuBox__menuList">
-                            <li class="header__box__menuBox__menuList__item"><a href="#">HOME</a></li>
-                            <li class="header__box__menuBox__menuList__item"><a href="#">ABOUT</a></li>
-                            <li class="header__box__menuBox__menuList__item"><a href="#">SERVICES</a></li>
-                            <li class="header__box__menuBox__menuList__item"><a href="#">PORTFOLIO</a></li>
-                            <li class="header__box__menuBox__menuList__item"><a href="#">CONTACT</a></li>
-                        </ul>
+                    <div class="col-md-10">
+                        <div class="header__box__menuBox">
+                            <ul class="header__box__menuBox__menuList">
+                                <li class="header__box__menuBox__menuList__item"><a href="#">HOME</a></li>
+                                <li class="header__box__menuBox__menuList__item"><a href="#">ABOUT</a></li>
+                                <li class="header__box__menuBox__menuList__item"><a href="#">SERVICES</a></li>
+                                <li class="header__box__menuBox__menuList__item"><a href="#">PORTFOLIO</a></li>
+                                <li class="header__box__menuBox__menuList__item"><a href="#">CONTACT</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-md-2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="header__box__logoBox">
+                            <img class="header__box__logoBox__logo" src="images/logo.png" alt="logo">
+                            <p class="header__box__logoBox__text">
+                                Get to explore it!
+                            </p>
+                            <button class="header__box__logoBox__button">Learn More</button>
+                        </div>
+                    </div>
                 </div>
 
             </div>
